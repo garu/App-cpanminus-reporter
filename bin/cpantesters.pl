1562c6a64e4a0e76afa50e13255ecf63776993ef
Breno G. de Oliveira <garu@cpan.org>
2012-04-08 03:21:26 -0300

    moar docs :)

--- bin/cpantesters.pl
+++ bin/cpantesters.pl
@@ -14,3 +14,21 @@ my $tester = App::cpantesters->new;
 $tester->run();
 
 __END__
+
+=head1 WARNING: WORK IN PROGRESS AHEAD
+
+This is a work in progress for now. If you care, please look for garu, xdg, barbie or miyagawa on irc.perl.org. Cheers.
+
+=head1 SYNOPSIS
+
+Basic usage (for now, meaning developers only!!):
+
+   > mkdir /tmp/reporter
+
+   > cpanm Some::Module
+   (wait for it to finish)
+
+   > cpantesters
+
+Then check out STDERR for messages, and /tmp/reporter to see the emails that will be sent to CPAN Testers!
+
